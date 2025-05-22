1e2540c63ea592fbba1ce246f0223a6f0f74a9fa
Fathima Shakki K C <fathimashakki21@gmail.com>
2025-05-22 10:16:48 +0530

    Initial Commit

---
+++ neumorphism/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  neumorphism
+//
+//  Created by Shaan on 22/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ neumorphism/neumorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  neumorphismApp.swift
+//  neumorphism
+//
+//  Created by Shaan on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct neumorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
